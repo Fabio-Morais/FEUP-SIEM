65e0130b71402c8206f5c6f399d592ce73f0b6e1
Fábio Morais <fabiouds@hotmail.com>
2020-10-14 23:33:46 +0100

    :100: FINAL VERSION

--- js/main.js
+++ js/main.js
@@ -1,25 +1,59 @@
 
+/*mostra o modal apenas 1 vez*/
+$(document).ready(function () {
+  var Modal = document.getElementById('id1');
+  var key = 'check',
+    hadModal = localStorage.getItem(key);
+  if (!hadModal) {
+    Modal.style.display = "block";
+    localStorage.setItem(key, true);
+  }
+});
 
 
 
-var map = L.map('map',{
-    center: [41.178346, -8.595829],
-    zoom: 15,
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: ''
-    }).addTo(map);
-
-    var firefoxIcon = L.icon({
-      iconUrl: 'img/icon.png',
-      iconSize: [48, 55], // size of the icon
-      });
-      map.scrollWheelZoom.disable();
-      map.on('focus', () => { map.scrollWheelZoom.enable(); });
-      map.on('blur', () => { map.scrollWheelZoom.disable(); });
-  var marker = L.marker([41.178346, -8.595829], {icon: firefoxIcon}).addTo(map).bindPopup('<h3><b>ExplicaFeup</b></h3><br><b>Phone: </b>(+351) 926789452<br><b>Email: </b>explicaFeup@example.com<br> <b>Adress: </b> R. Dr. Roberto Frias, 4200-465 Porto').addTo(map);;
+/*FUNÇAO QUE CONTA DE start ATE end*/
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
 
 
 
-  
+/* ANIMAÇAO DA SECÇAO "NUMEROS"*/
+var breakpoint = false;
+window.addEventListener('scroll', function () {
+  if (!breakpoint) {
+    var element = document.querySelector('#numberIcon');
+    if(element != null){
+      var position = element.getBoundingClientRect();
+
+      /*Se mostrar o <div> dos numeros começa o calculo */
+      if (position.top < window.innerHeight && position.bottom >= 0) {
+        console.log('Element is partially visible in screen');
+        breakpoint = true;
+        const obj = document.getElementById('countCourse');
+        animateValue(obj, 0, 100, 2000);
+        const obj2 = document.getElementById('countClients');
+        animateValue(obj2, 0, 200, 2500);
+        const obj3 = document.getElementById('countYears');
+        animateValue(obj3, 0, 10, 1500);
+      }
+    }
+
+  }
+});
+
+
+/* ATIVA ANIMAÇOES DE ENTRADA */
+AOS.init();
+
+
