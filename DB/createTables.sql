fcb6fc3518fa1cbc3aa650d81654bdf449853c15
fábio <fabiouds@hotmail.com>
2020-11-08 18:49:49 +0000

    update

--- DB/createTables.sql
+++ DB/createTables.sql
@@ -8,23 +8,7 @@ CREATE TABLE IF NOT EXISTS Cliente(
     nif VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS Encomenda(
-    id SERIAL PRIMARY KEY,
-	numero VARCHAR(100),
-    dataEntrega TIMESTAMP,
-    dataDeCompra TIMESTAMP,
-    preco VARCHAR(100)
-);
-
-
-CREATE TABLE IF NOT EXISTS Produto(
-	id SERIAL PRIMARY KEY,
-	nome VARCHAR(100),
-    descricao VARCHAR(100),
-    preco VARCHAR(15) default 0
-);
-
-CREATE TABLE IF NOT EXISTS User(
+CREATE TABLE IF NOT EXISTS Userr(
     userName VARCHAR(100) PRIMARY KEY,
     email VARCHAR(100),
     telefone VARCHAR(100),
@@ -36,13 +20,24 @@ CREATE TABLE IF NOT EXISTS User(
 );
 
 CREATE TABLE IF NOT EXISTS Aluno(
-    id SERIAL PRIMARY KEY,
-	nota FLOAT default 0
+    username VARCHAR(100) REFERENCES Userr(userName) PRIMARY KEY,
+	notaAluno FLOAT default 0
 );
 
 CREATE TABLE IF NOT EXISTS Professor(
-    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) REFERENCES Userr(userName) PRIMARY KEY,
 	salario FLOAT default 0
+);
+
+CREATE TABLE IF NOT EXISTS Encomenda(
+    numero SERIAL PRIMARY KEY,
+    dataEntrega TIMESTAMP,
+    dataDeCompra TIMESTAMP,
+    preco VARCHAR(100),
+    nomeProduto VARCHAR(100),
+    descricao VARCHAR(100),
+    idCliente VARCHAR(100) REFERENCES Cliente(id) ON DELETE CASCADE,
+    idAluno VARCHAR(100) REFERENCES Aluno(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Salario(
@@ -52,9 +47,16 @@ CREATE TABLE IF NOT EXISTS Salario(
 );
 
 CREATE TABLE IF NOT EXISTS Curso(
-    nome VARCHAR(50) PRIMARY KEY,
-);        
+    nome VARCHAR(50) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Enrolled(
+    idAluno VARCHAR(100) REFERENCES Aluno(id),
+    nomeCurso VARCHAR(100) REFERENCES Curso(nome),
+    notaCurso FLOAT default 0,
+    PRIMARY KEY(idAluno, nomeCurso)
+);            
 
 CREATE TABLE IF NOT EXISTS Admin(
-    userName VARCHAR(50) PRIMARY KEY,
+    userName VARCHAR(50) PRIMARY KEY
 );        
