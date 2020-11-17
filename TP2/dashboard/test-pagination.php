99ddda5cc9324a136e3ef0ab9cf17bab6c9cfe08
Fábio Morais <fabiouds@hotmail.com>
2020-11-17 00:59:40 +0000

    template dos users, com pagination e search bar

--- TP2/dashboard/test-pagination.php
+++ TP2/dashboard/test-pagination.php
@@ -1,36 +1,63 @@
-</main>
-<footer class="py-4 bg-light mt-auto">
-    <div class="container-fluid">
-        <div class="align-items-center justify-content-between small">
-            <div class="text-muted text-center">Copyright &copy; Fábio Morais e Fernando Silva 2020</div>
-        </div>
-    </div>
-</footer>
-</div>
-</div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="public/js/scripts.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="assets/demo/chart-area-demo.js"></script>
-<script src="assets/demo/chart-bar-demo.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-<script src="assets/demo/datatables-demo.js"></script>
-<script src="public/js/main.js"></script>
-<script>
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".box").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-</script>
+<!DOCTYPE html>
+<html>
 
-<script>
-    
+<head>
+    <!-- Font Awesome -->
+    <link href="public/css/styles.css" rel="stylesheet" />
+    <link href="public/css/main.css" rel="stylesheet" />
+    <link href="../public/css/profile.css" rel="stylesheet" />
+
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    <title>
+
+    </title>
+</head>
+
+<body>
+
+    <div class="pagination">
+    </div>
+
+    <div id="jar" style="display:none">
+        <div class="content">
+            <div class="card"> asdsaddsds22</div>
+        </div>
+        <div class="content">
+            <div class="card"> asdsad</div>
+        </div>
+        <div class="content">3) Pellentesque sagittis risus ac ante ultricies, ac convallis urna elementum.</div>
+        <div class="content">4) Vivamus sodales aliquam massa quis lobortis. </div>
+        <div class="content">5) Phasellus id sem sollicitudin lacus condimentum malesuada vel tincidunt neque.</div>
+        <div class="content">6) Donec magna leo, rhoncus quis nunc eu, malesuada consectetur orci.</div>
+        <div class="content">7) Praesent sollicitudin, quam a ullamcorper pharetra, urna lacus mollis sem, quis semper augue massa ac est.</div>
+        <div class="content">8) Etiam leo magna, fermentum quis quam non, aliquam tincidunt erat.</div>
+        <div class="content">9) Morbi pellentesque nibh nec nibh posuere, vel tempor magna dignissim.</div>
+        <div class="content">10) In maximus fermentum elementum. Vestibulum ac lectus pretium, suscipit ante nec, bibendum erat.</div>
+        <div class="content">11) Phasellus sit amet orci at lectus fermentum congue. Etiam faucibus scelerisque purus.</div>
+        <div class="content">12) Pellentesque laoreet ipsum ac laoreet consectetur. </div>
+        <div class="content">13) Integer aliquet odio magna, lobortis mattis tortor suscipit sed.</div>
+        <div class="content">14) Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
+        <div class="content">15) Mauris a tellus luctus turpis elementum imperdiet vitae malesuada mauris. </div>
+        <div class="content">16) Donec id libero sagittis, laoreet lorem vel, tempus nunc. </div>
+        <div class="content">17) Donec vitae neque sed ex tristique hendrerit.</div>
+        <div class="content">18) Aliquam sollicitudin gravida varius.</div>
+        <div class="content">19) Donec auctor, augue sed finibus fermentum, neque erat interdum libero, eget porta metus lectus quis odio.</div>
+        <div class="content">20) Nunc quis ante enim. Etiam nisl orci, hendrerit ut pretium nec, tempor in metus.</div>
+
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="public/js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+    <script src="public/js/main.js"></script>
+    <script>
         // Returns an array of maxLength (or less) page numbers
         // where a 0 in the returned array denotes a gap in the series.
         // Parameters:
@@ -66,23 +93,12 @@ $(document).ready(function(){
                 .concat(0, range(page - leftWidth, page + rightWidth),
                     0, range(totalPages - sideWidth + 1, totalPages));
         }
-        $(window).on('resize', function(){
 
         // Below is an example use of the above function.
         $(function() {
             // Number of items and limits the number of items per page
             var numberOfItems = $("#jar .content").length;
-            var limitPerPage = 8;
-            if(window.innerWidth >= 1770){
-                limitPerPage = 10;
-            }
-            else if((window.innerWidth < 1770) && (window.innerWidth >= 1330)){
-                limitPerPage = 8;
-            }
-            else if(window.innerWidth < 1330){
-                limitPerPage = 4;
-            }
-            console.log(window.innerWidth)
+            var limitPerPage = 4;
             // Total pages rounded upwards
             var totalPages = Math.ceil(numberOfItems / limitPerPage);
             // Number of buttons at the top, not counting prev/next,
@@ -147,7 +163,6 @@ $(document).ready(function(){
                 return showPage(currentPage - 1);
             });
         });
-    });
     </script>
 
 </body>
--- TP2/dashboard/users.php
+++ TP2/dashboard/users.php
@@ -4,15 +4,303 @@
 <?php require_once(dirname(__FILE__) . "/templates/common/title.php"); ?>
 <div class="container-fluid">
     <div class="justify-content-center m-4">
-        <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card border-left-primary shadow h-100 py-2" >
             <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    Users
+            <form class="form-inline d-flex justify-content-center md-form form-sm active-cyan-2 mt-2">
+            <i class="fas fa-search" aria-hidden="true"></i>
+            <input class="form-control form-control-sm ml-2 w-75" type="text" placeholder="Search"
+                aria-label="Search" id="myInput">
+            </form>
+                <div class="d-flex flex-wrap no-gutters justify-content-around"  id="jar" style="display:none">
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-aqua">
+                                <h3 class="widget-user-username text-center">fabio Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                                <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">3</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">--</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-red">
+                                <h3 class="widget-user-username text-center">rita antunes</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">19</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Ana Morais</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">5</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">--</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Professor</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">3</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">--</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">1</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">15</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">2</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Joao Silva</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">2</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">2</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">2</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 m-3 content">
+                        <div class="box box-widget widget-user">
+                            <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username text-center">Alexander Pierce</h3>
+                            </div>
+                            <div class="widget-user-image">
+                                <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="User Avatar">
+                            </div>
+                            <div class="box-footer ">
+                            <h5 class="widget-user-desc text-center ">Aluno</h5>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="description-block text-center">
+                                            <h5 class="description-header">2</h5>
+                                            <span class="description-text">Cursos</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm">
+                                        <div class="description-block">
+                                            <h5 class="description-header">10</h5>
+                                            <span class="description-text">Nota</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+                <div class="pagination">
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
 
 <?php require_once(dirname(__FILE__) . "/templates/common/footer.php"); ?>
