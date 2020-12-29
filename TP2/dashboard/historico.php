dd908703d4868b68d7a8c48117da12e9283a67dd
Fábio Morais <fabiouds@hotmail.com>
2020-12-29 23:03:25 +0000

    juntar data de vendas

--- TP2/dashboard/historico.php
+++ TP2/dashboard/historico.php
@@ -30,7 +30,7 @@ if ($db->connect()) {
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <table class="table table-bordered " id="dataTable" width="100%" cellspacing="0">
                     <thead>
                     <tr>
                         <th>Data de Compra</th>
@@ -63,3 +63,4 @@ if ($db->connect()) {
 
 </div>
 <?php require_once(dirname(__FILE__) . "/templates/common/footer.php"); ?>
+
